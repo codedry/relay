1c87f47411559967d500cc3af99c3eb9b85adf92
Kenrick Beckett <kenrick.beckett@gmail.com>
2014-10-19 16:19:56 -0500

    Add notes to tickets

--- db/migrate/20141018183124_create_tickets.rb
+++ db/migrate/20141018183124_create_tickets.rb
@@ -3,6 +3,7 @@ class CreateTickets < ActiveRecord::Migration
     create_table :tickets do |t|
       t.string  :name
       t.string  :code
+      t.text    :notes
       t.integer :status
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141019212315) do
   create_table "tickets", force: true do |t|
     t.string   "name"
     t.string   "code"
+    t.text     "notes"
     t.integer  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
