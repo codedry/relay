cd6c115a43423c9ba886903f193f10f97cf43247
Kenrick Beckett <kenrick.beckett@gmail.com>
2014-10-19 16:48:17 -0500

    Add Ticket Controller and views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,11 @@
 class Ticket < ActiveRecord::Base
+  enum status: [ :pending, :ready ]
+
+  before_create :generate_code
+
+  private
+    def generate_code
+      self.code = (0...6).map { (65 + rand(26)).chr }.join
+    end
+
 end
---
+++ app/views/tickets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @ticket, :id, :name, :notes, :created_at, :updated_at
